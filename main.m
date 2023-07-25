3504098fd15e7638a13ca954ef5e6cf2f4417ade
blockbuster12921 <blockbuster12921@gmail.com>
2023-07-24 22:50:27 -0500

    disp added

--- main.m
+++ main.m
@@ -53,6 +53,7 @@ title('Correlation plot');
 % Save the heatmap as an image
 saveas(h, 'corrcoef.png');
 
+disp('Task2 Result:')
 disp(max(cor(1, :)));
 disp(min(cor(1, :)));
 
@@ -77,13 +78,15 @@ B = mnrfit(X_train, y_train);
 probabilities = mnrval(B, X_test);
 predictions = probabilities(:, 2) > 0.5; % Assuming binary classification
 
+
+disp('Task3 Result:')
 % Calculating accuracy rate
-accuracy = sum(predictions == y_test) / numel(y_test);
-fprintf('Accuracy Rate: %.2f%%\n', accuracy * 100);
+accuracy_3 = sum(predictions == y_test) / numel(y_test) * 100;
+fprintf('Accuracy Rate: %.2f%%\n', accuracy_3);
 
 % Calculating confusion matrix
-confusionMatrix = confusionmat(y_test, double(predictions));
-disp(confusionMatrix);
+confusionMatrix_3 = confusionmat(y_test, double(predictions));
+disp(confusionMatrix_3);
 
 %% Task4
 % Use the training sample and a logistic regression model which only included
@@ -108,13 +111,15 @@ B = mnrfit(X_train, y_train);
 probabilities = mnrval(B, X_test);
 predictions = probabilities(:, 2) > 0.5; % Assuming binary classification
 
+disp('Task4 Result:')
+
 % Calculating accuracy rate
-accuracy = sum(predictions == y_test) / numel(y_test);
-fprintf('Accuracy Rate: %.2f%%\n', accuracy * 100);
+accuracy_4 = sum(predictions == y_test) / numel(y_test) * 100;
+fprintf('Accuracy Rate: %.2f%%\n', accuracy_4);
 
 % Calculating confusion matrix
-confusionMatrix = confusionmat(y_test, double(predictions));
-disp(confusionMatrix);
+confusionMatrix_4 = confusionmat(y_test, double(predictions));
+disp(confusionMatrix_4);
 
 %% Task5
 % Use a boosted classification tree to train the model and then, similarly, report
@@ -133,36 +138,41 @@ treeMdl = fitcensemble(X_train, y_train, 'Method', 'AdaBoostM1', 'Learners', t);
 % Predict the result for the test data
 predictions = predict(treeMdl, X_test);
 
+disp('Task5 Result:')
+
 % Calculating accuracy rate
-accuracy = sum(predictions == y_test) / numel(y_test);
-fprintf('Accuracy Rate: %.2f%%\n', accuracy * 100);
+accuracy_5 = sum(predictions == y_test) / numel(y_test) * 100;
+fprintf('Accuracy Rate: %.2f%%\n', accuracy_5);
 
 % Calculating confusion matrix
-confusionMatrix = confusionmat(y_test, predictions);
-disp(confusionMatrix);
+confusionMatrix_5 = confusionmat(y_test, predictions);
+disp(confusionMatrix_5);
 
-% %% Task6
-% % Use a random forest to train the model and then, similarly, report the OOS
-% % confusion matrix and the accuracy rate.
-% 
-% % Split the data into X_train, y_train, X_test, y_test
-% X_train = table2array(train_table(:, 2:end));
-% y_train = categorical(table2array(train_table(:, 1)));
-% X_test = table2array(test_table(:, 2:end));
-% y_test = categorical(table2array(test_table(:, 1)));
-% 
-% t = templateTree('MaxNumSplits', 5, 'PredictorSelection','interaction-curvature','Reproducible', true);
-% rfMdl = fitcensemble(X_train, y_train, 'Method', 'Bag', 'NumLearningCycles', 50, 'Learners', t);
-% predictions = predict(boostMdl, X_test);
-% 
-% % Calculating accuracy rate
-% accuracy = sum(predictions == y_test) / numel(y_test);
-% fprintf('Accuracy Rate: %.2f%%\n', accuracy * 100);
-% 
-% % Calculating confusion matrix
-% confusionMatrix = confusionmat(y_test, predictions);
-% disp(confusionMatrix);
+%% Task6
+% Use a random forest to train the model and then, similarly, report the OOS
+% confusion matrix and the accuracy rate.
 
+% Split the data into X_train, y_train, X_test, y_test
+X_train = table2array(train_table(:, 2:end));
+y_train = categorical(table2array(train_table(:, 1)));
+X_test = table2array(test_table(:, 2:end));
+y_test = categorical(table2array(test_table(:, 1)));
+
+t = templateTree('MaxNumSplits', 5, 'PredictorSelection','interaction-curvature','Reproducible', true);
+rfMdl = fitcensemble(X_train, y_train, 'Method', 'Bag', 'NumLearningCycles', 50, 'Learners', t);
+predictions = predict(rfMdl, X_test);
+
+disp('Task6 Result:')
+
+% Calculating accuracy rate
+accuracy_6 = sum(predictions == y_test) / numel(y_test) * 100;
+fprintf('Accuracy Rate: %.2f%%\n', accuracy_6);
+
+% Calculating confusion matrix
+confusionMatrix_6 = confusionmat(y_test, predictions);
+disp(confusionMatrix_6);
+save('save.mat', 'accuracy_3', 'confusionMatrix_3', 'accuracy_4', 'confusionMatrix_4', ...
+    'accuracy_5', 'confusionMatrix_5', 'accuracy_6', 'confusionMatrix_6');
 %% Task7
 % Compare the results from different models. Comment on your findings.
 
